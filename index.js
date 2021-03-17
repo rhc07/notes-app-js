63294e168d2a71816db9f7929cbfc73bcff32920
sandy <pounoussamy.sandy@gmail.com>
2021-03-17 17:35:49 +0000

    refactoring

--- index.js
+++ index.js
@@ -13,29 +13,26 @@ function insertNote(content) {
   noteList.push(content)
 }
 
-function getContentValue() {
-  var value = document.getElementById("note").value;
-  return value;
+
+function getBoxContent() {
+  let content = document.getElementById("note-content").value;
+  return content;
 }
-function createElement(content) {
-  var el = document.createElement("a");
-  var notePad = document.getElementsByClassName("notes")[0];
-  notePad.appendChild(el);
-  el.setAttribute("href", '#' + content);
-  return el;
-}
-function appendText(content, element) {
-  element.innerHTML = "<li>" + content + "</li>";
+
+function printContent(content) {
+  document.getElementById("notes").innerHTML += (`<a href='#${content}'>` + content + '</a>');
 }
 
 function addNote() {
-  var content = getContentValue();
-  insertNote(content);
-  appendText(content, createElement(content));
-  resetTextarea();
+  printContent(getBoxContent());
 }
 
-function resetTextarea() {
-  var text = document.getElementById('note');
-  text.value = text.defaultValue;
+function eventHandler() {
+  let button = document.getElementById("submit");
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
+    addNote();
+});
 }
+
+eventHandler();
