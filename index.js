8e7c3211dc52f18a2724b74e9a06f46a148d3ef6
sandy <pounoussamy.sandy@gmail.com>
2021-03-18 10:33:15 +0000

    Added model

--- index.js
+++ index.js
@@ -1,38 +1,81 @@
-// function create() {
-//   var value = document.getElementById("note").value;
-//   var el = document.createElement("a");
-//   var notePad = document.getElementsByClassName("notes")[0];
-//   notePad.appendChild(el);
-//   el.setAttribute("href", '#' + value)
-//   el.innerHTML = "<li>" + value + "</li>";
-// } 
-
-noteList = [];
-
-function insertNote(content) {
-  noteList.push(content)
+class Note {
+  constructor(content) {
+    this.content = content;
+  }
 }
 
+class NoteManager {
+  constructor() {
+    this.notes = []
+  }
+}
 
+// Initialising NoteManager instance
+myNoteManager = new NoteManager;
+
+
+// Prints the notes from the NoteManager onto the HTML
+function printNote(note) {
+  document.getElementById("notes").innerHTML += (`<a href='#${note.content}'>` + note.content.substring(0,20) + '...' + '</a>');
+}
+
+function printContent() {
+  clearNotePad();
+  myNoteManager.notes.forEach(note => {
+    printNote(note);
+  });
+}
+
+function clearNotePad() {
+  document.getElementById("notes").innerHTML = '';
+}
+
+// Add the note to NoteManager instance
 function getBoxContent() {
   let content = document.getElementById("note-content").value;
+  let note = new Note(content);
+  insertNote(note);
   return content;
 }
 
-function printContent(content) {
-  document.getElementById("notes").innerHTML += (`<a href='#${content}'>` + content + '</a>');
+function insertNote(content) {
+  myNoteManager.notes.push(content)
+}
+
+function clearTextarea() {
+  document.getElementById("note-content").value = "";
 }
 
 function addNote() {
-  printContent(getBoxContent());
+  getBoxContent();
+  clearTextarea();
 }
 
+// Local storage management --> this is not working yet
+// function storeData() {
+//   localStorage.setItem('notes', JSON.stringify(myNoteManager.notes));
+// }
+
+// function retrieveData() {
+//   let retrievedData = localStorage.getItem('notes');
+//   let data = JSON.parse(retrievedData);
+//   data.forEach(note => myNoteManager.notes.push(new Note(note)));
+//   printContent();
+// }
+
+// Event Handler
 function eventHandler() {
   let button = document.getElementById("submit");
   button.addEventListener('click', function(event) {
     event.preventDefault();
     addNote();
+    printContent()
+
+    window.addEventListener("load", retrieveData());
+    window.addEventListener("beforeunload", storeData());
 });
 }
 
 eventHandler();
+
+
